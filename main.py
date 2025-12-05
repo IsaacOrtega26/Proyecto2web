c44990eccb59067aeeffe081eb658f884ccb3470
IsaacOrtega26 <isaacd260624@gmail.com>
2025-12-04 20:02:05 -0600

    Se hizo codigo del archivo main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(filename="logs/scraper.log",
+                level=logging.INFO,
+                format="%(asctime)s - %(message)s")
+
+logging.info("Scraping iniciado")
