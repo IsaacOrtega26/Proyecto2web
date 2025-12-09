ff6736a3cd420d8be2f8e1f8a6fd0561ba658a2f
vane12-j <erickavanessaamadorjimenezva@gmail.com>
2025-12-09 10:50:11 -0600

    se mejoro el json_api_server

--- api/json_api_server.py
+++ api/json_api_server.py
@@ -3,12 +3,33 @@ import psycopg2
 
 app = Flask(__name__)
 
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        port=5434,
+        user="admin",
+        password="admin",
+        database="scrapingdb"
+    )
+
 @app.get("/results")
 def get_results():
-    conn = psycopg2.connect(host="localhost", user="admin", password="admin", database="scrapingdb")
+    conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM products")
-    data = cursor.fetchall()
-    return jsonify(data)
+    cursor.execute("SELECT id, title, price, url, created_at FROM products;")
+    rows = cursor.fetchall()
+    conn.close()
+
+    products = []
+    for r in rows:
+        products.append({
+            "id": r[0],
+            "title": r[1],
+            "price": float(r[2]) if r[2] else None,
+            "url": r[3],
+            "created_at": str(r[4])
+        })
+
+    return jsonify(products)
 
 app.run(debug=True, port=5000)
